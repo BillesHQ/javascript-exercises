bd7c20365a32116764774e66eccf9d963273f0c0
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-03 11:19:41 -0500

    Edit 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(num,remover) {
+    let array = []
+    for (let i = 0; i < num.length; i++){
+        if (num[i] !== remover) {
+                array.push(num[i])
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
