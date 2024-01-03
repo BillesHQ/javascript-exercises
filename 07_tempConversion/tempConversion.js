7ceabaa7b55e41346cda0ddcedd419b7153d2144
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-03 14:16:45 -0500

    Edit tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  f = (f-32)/1.8
+  return Math.round(10*f)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+ c = (c * 9/5) + 32
+ return Math.round(10*c)/10
 };
 
 // Do not edit below this line
