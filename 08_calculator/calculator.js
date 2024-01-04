78f172dfa4c0ac82b6f972153d5863c4e387b420
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-04 11:47:01 -0500

    Edit calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(min,sub) {
+	return min - sub
 };
 
-const sum = function() {
-	
+const sum = function(add1,add2) {
+	return add1 + add2
 };
 
-const multiply = function() {
-
+const multiply = function(mul1,mul2) {
+  return mul1 * mul2
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	return Math.pow(num,exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(start,end) {
+  let proudct = 1
+  for (let i = start; i <= end; i ++){
+    proudct *= i
+    }
+  return proudct
 };
 
 // Do not edit below this line
