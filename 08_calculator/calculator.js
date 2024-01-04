ea9ac0d3a19d5aa21e839affbdc89b98544dd1ad
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-04 14:06:30 -0500

    Edit calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,5 +1,10 @@
 const add = function(num1,num2) {
-	return num1 + num2
+  let sum = 0
+  for (let i =0; i <= num1.length;i ++){
+    sum += [i]
+  }
+
+  return sum + num2
 };
 
 const subtract = function(min,sub) {
@@ -7,11 +12,19 @@ const subtract = function(min,sub) {
 };
 
 const sum = function(add1,add2) {
-	return add1 + add2
+    let sum = 0
+    for (let i =0; i < num1.length; i++){
+        sum += num1[i];
+  }
+    return sum + num2
 };
 
 const multiply = function(mul1,mul2) {
-  return mul1 * mul2
+  let product = 1
+  for (let i = 0; i < mul1.length; i++){
+    product *= mul1[i]
+  }
+  return product * mul2
 };
 
 const power = function(num, exponent) {
