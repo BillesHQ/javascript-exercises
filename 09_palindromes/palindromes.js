24155589e95fa675976cda576f7d4607bcbb599b
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-05 15:31:29 -0500

    Edit palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase()
+    let result = '';
+    for (let i = newString.length- 1; i >= 0; i--){
+       result += newString[i];
+    }
+  return result === newString
 };
 
 // Do not edit below this line
