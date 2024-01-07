fca37cacd2bbc64aafd987919fb8eaca750ff647
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-06 23:13:07 -0500

    Edit palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,8 @@ const palindromes = function (string) {
     let newString = string.toLowerCase()
     let result = '';
     for (let i = newString.length- 1; i >= 0; i--){
-       result += newString[i];
+       if (string === ',') continue;
+        result += newString[i];
     }
   return result === newString
 };
