ff90c4b4b3753dc8a470a4001040976fdd37431f
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-09 13:42:25 -0500

    Edit fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num <= 2) {
+        return 1
+    }
+    let first = 1;
+    let second = 1;
+    let last;
+    for (let i = 2; i < num; i++) {
 
+        last = first + second;
+
+        first = second;
+        second = last;
+    }
+    return last
 };
 
 // Do not edit below this line
