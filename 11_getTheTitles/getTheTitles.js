a0410931e155f903ee1015c27551d689a204bab8
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-10 13:31:19 -0500

    Edit getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -5,6 +5,10 @@ const getTheTitles = function(array) {
     }
     return titles
 };
-
+// const getTheTitles2 = function(array) {
+//     return array.map(function(book) {
+//         return book.title;
+//     });
+//  };
 // Do not edit below this line
 module.exports = getTheTitles;
