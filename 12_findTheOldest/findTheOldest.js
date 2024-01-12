8c074889098b2ae526317d90cfa592826acceef0
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-01-12 00:06:52 -0500

    Edit findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // let years = [];
+    let currentYear = new  Date().getFullYear()
+    for (let i =0; i < people.length; i++) {
+        if (!people[i].yearOfDeath && people[i].yearOfDeath != typeof 'number'){
+            // years.push(currentYear - people[i].yearOfBirth);
+            people[i].age = (currentYear - people[i].yearOfBirth)
+        } else {
+            years[i].age = (people[i].yearOfDeath - people[i].yearOfBirth)
+        }
+    }
+
+
+
+    // let ages = years.sort()
+    // let answer = ages.reduce(ages
+    return Math.max(...people.ages)
+    // let person = people.find(p => p.)
+
+
 
 };
 
