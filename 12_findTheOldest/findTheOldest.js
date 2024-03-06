ea7b50809838746b1fd54cdbf5cac5887d9dd7fa
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-03-06 13:15:09 -0500

    edit complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -4,26 +4,35 @@ const findTheOldest = function(people) {
     for (let i =0; i < people.length; i++) {
         if (!people[i].yearOfDeath && people[i].yearOfDeath != typeof 'number'){
             years.push(currentYear - people[i].yearOfBirth);
-            // people[i].age = (currentYear - people[i].yearOfBirth)
         } else {
             years.push(people[i].yearOfDeath - people[i].yearOfBirth)
-            // people[i].age = (people[i].yearOfDeath - people[i].yearOfBirth)
         }
     }
 
-    // for (let i = 1; i < people.length; i++){
-    //     let first = people[i+1].age
-    //     let second = people[i].age
-    //     let last = first < second
-    //     first = second
-    //     second = last
-    // let ages = years.sort()
-    // let ages = Math.max(...years).indexOf()
+
     let index = years.indexOf(Math.max(...years));
-    // // let answer = ages.reduce(ages
-    // }
+
     return people[index]
-    // let person = people.find(p => p.)
+
+};
+
+function findTheOldest2(people) {
+    let maxAge = -1;
+    let maxPerson;
+    let currentYear = new  Date().getFullYear();
+    for (let i = 0; i < people.length; i++) {
+        let age;
+        if (!people[i].yearOfDeath && people[i].yearOfDeath != typeof 'number'){
+            age = currentYear - people[i].yearOfBirth
+        } else {
+            age = people[i].yearOfDeath - people[i].yearOfBirth
+        }
+        if (age > maxAge) {
+            maxAge = age;
+            maxPerson = people[i];
+        }
+    }
+    return maxPerson
 };
 
 // Do not edit below this line
